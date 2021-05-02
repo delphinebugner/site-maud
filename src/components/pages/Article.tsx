b144ceab8d26020db51d193ae37263800f0cc604
Delphine Bugner <delphineb@bam.tech>
2021-05-02 16:43:56 +0200

    :art: (Article) add separators & realign text

--- src/components/pages/Article.tsx
+++ src/components/pages/Article.tsx
@@ -4,6 +4,7 @@ import { Layout } from "../Layout";
 import { Language } from "../../lib/language";
 import { Content } from "../../lib/types";
 import { DateComponent } from "../Date";
+import { Separator } from "../Separator";
 
 export interface Props {
   content: Content<ArticleAttributes>;
@@ -22,13 +23,13 @@ export const Article: NextPage<Props> = ({ content, language }) => {
   console.log(attributes.date, typeof attributes.date);
   return (
     <Layout language={language}>
-      <div className="flex flex-col items-center justify-center h-full px-48">
+      <div className="flex flex-col h-full px-48">
         <span className="font-bold text-6xl text-blue-600">
           {attributes.title}
         </span>
-        <div className="h-2 w-full" />
-        <p className="text-5xl">{attributes.subtitle}</p>
-        <div className="h-2 w-full" />
+        <Separator h={2} />
+        <p className="text-2xl">{attributes.subtitle}</p>
+        <Separator h={4} />
         <DateComponent date={attributes.date} language={language} />
         <div className="prose">{parse(html)}</div>
       </div>
