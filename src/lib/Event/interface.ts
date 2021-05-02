9a4b2cf6f6d0ad66b673d88709245840e4e2eb9c
Delphine Bugner <delphineb@bam.tech>
2021-05-02 22:31:57 +0200

    :sparkles: (lib Event) create module

---
+++ src/lib/Event/interface.ts
@@ -0,0 +1,19 @@
+export interface RawEvent {
+  id: string;
+  name_fr: string;
+  name_en: string;
+  place: string;
+  date: string;
+  hour?: string;
+  body_fr: string;
+  body_en: string;
+}
+
+export interface Event {
+  id: string;
+  name: string;
+  place: string;
+  date: string;
+  hour?: string;
+  body: string;
+}
