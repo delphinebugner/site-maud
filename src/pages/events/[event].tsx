2ad696ce16dd2990b26b5bf5ebfe86696a639348
Delphine Bugner <delphineb@bam.tech>
2021-05-02 22:32:35 +0200

    :sparkles: (event) add Next pages

---
+++ src/pages/events/[event].tsx
@@ -0,0 +1,32 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import {
+  EventPage,
+  Props as EventPageProps,
+} from "../../components/pages/Event";
+import { fetchEvent, fetchEventsPaths } from "../../lib/Event/api";
+import { EN } from "../../lib/language";
+
+const language = EN;
+
+export const getStaticProps: GetStaticProps<EventPageProps> = async ({
+  params,
+}) => {
+  const eventId = params.event as string;
+  const event = fetchEvent(language, eventId);
+  return {
+    props: {
+      language,
+      event,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = fetchEventsPaths(language);
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default EventPage;
