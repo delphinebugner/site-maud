1ef076ee73b8b538d3ef3afba115eb1e7a838c0a
Delphine Bugner <delphineb@bam.tech>
2021-05-02 22:31:21 +0200

    :sparkles: (Event & EventList) create components

---
+++ src/components/pages/EventList.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { NextPage } from "next";
+import { Layout } from "../Layout";
+import { getUrlPrefix, Language } from "../../lib/language";
+import { Event } from "../../lib/Event/interface";
+import { EVENTS } from "../../lib/routes";
+
+export interface Props {
+  events: Event[];
+  language: Language;
+}
+export const EventListPage: NextPage<Props> = ({ events, language }) => {
+  return (
+    <Layout language={language}>
+      <div className="flex flex-col items-center justify-center h-full">
+        <div>
+          {events.map(({ name, place, date, id }, i) => (
+            <Link key={i} href={`${getUrlPrefix(language)}${EVENTS}/${id}`}>
+              <a className="flex flex-col cursor-pointer hover:opacity-80 transition-all duration-500">
+                <p className="text-xl">{name}</p>
+                <p>{place}</p>
+                <p className="italic">{date}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
