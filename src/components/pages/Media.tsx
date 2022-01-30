1933d4f0ec4eead5a83da341d012d781684b3dcf
Delphine Bugner <delphineb@bam.tech>
2022-01-30 16:34:50 +0100

    :sparkles: feat: (Media) add one video

--- src/components/pages/Media.tsx
+++ src/components/pages/Media.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { EN, Language } from "../../lib/LanguageContext";
+import ReactPlayer from "react-player/youtube";
 import { Content } from "../../lib/types";
 import { Layout } from "../Layout";
 
@@ -22,6 +23,7 @@ export const Media: FunctionComponent<Props> = ({ content, language }) => {
             ? content.attributes.body_en
             : content.attributes.body_fr}
         </span>
+        <ReactPlayer url="https://www.youtube.com/watch?v=QaiP5TMsczw&ab_channel=EnsembleParnassoinFesta" />
       </div>
     </Layout>
   );
