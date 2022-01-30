9e734af2f78c42d143abbd072eab42a7e823d2c4
Delphine Bugner <delphineb@bam.tech>
2022-01-30 17:32:24 +0100

    :wrench: config:  va savoir

--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -3,7 +3,7 @@ import { Media, Props as MediaProps } from "../components/pages/Media";
 import { EN } from "../lib/LanguageContext";
 
 export const getStaticProps: GetStaticProps<MediaProps> = async () => {
-  const content = await import(`../../content/pages/media.md`);
+  const content = await import(`../../content/pages/media${""}.md`);
   return { props: { content: content.default, language: EN } };
 };
 export default Media;
